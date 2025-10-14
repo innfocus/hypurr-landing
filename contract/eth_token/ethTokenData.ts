3bf9542810bc2181eac9338cec57578752081b34
@quanhuy <64772864+nqhuy2509@users.noreply.github.com>
2025-10-14 16:08:19 +0700

    Update ethTokenData.ts

--- contract/eth_token/ethTokenData.ts
+++ contract/eth_token/ethTokenData.ts
@@ -1,7 +1,7 @@
 import { ChainId, Token } from '@uniswap/sdk-core'
 import { isMainnet } from '../../constants/constants'
 
-export const ETH_TOKEN_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const ETH_TOKEN_ADDRESS = '0x5555555555555555555555555555555555555555'
 
 export const ETH_TOKEN = new Token(
 	isMainnet ? ChainId.MAINNET : ChainId.SEPOLIA,
